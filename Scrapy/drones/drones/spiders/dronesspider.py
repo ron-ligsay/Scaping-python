f6e746d154c4ed364e07a7d4a8ac93813099ff7b
ron-ligsay <akyligsay16@gmail.com>
2023-03-03 18:01:01 +0800

    scrapy

--- Scrapy/drones/drones/spiders/dronesspider.py
+++ Scrapy/drones/drones/spiders/dronesspider.py
@@ -7,4 +7,11 @@ class DronesspiderSpider(scrapy.Spider):
     start_urls = ["http://jessops.com/"]
 
     def parse(self, response):
+        products = response.css('div.details-pricing')
+        for product in products:
+            item = {
+            'name' : product.css('a::text').get(),
+            'price' : product.css('p.price.larger::text').get()
+            }
+            yield item # return what it finds
         pass
